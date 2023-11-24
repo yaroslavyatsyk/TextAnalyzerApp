7ee350ac0d67c9052019c2637a576475530540bf
Yaroslav9728 <yaroslav9728@gmail.com>
2023-11-24 01:15:00 -0500

    Added a feature to generate pdf reports

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,6 +14,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using TextAnalyzerFinal;
+using iTextSharp;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Paragraph = iTextSharp.text.Paragraph;
+
 namespace TextAnalyzerApp
 {
     /// <summary>
@@ -132,6 +137,196 @@ namespace TextAnalyzerApp
                 System.Windows.MessageBox.Show("Can not save file!","Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
 
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            if(analyzerClass is not null)
+            {
+                NewMethod();
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("No text to save!","Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void NewMethod()
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog() { Filter = "PDF file|*.pdf", ValidateNames = true, FileName = "Text Analyzer Report - " + DateTime.Today.ToString("yyyy/MM/dd") })
+            {
+                if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    Document document = new Document();
+                    PdfWriter pdfWriter = PdfWriter.GetInstance(document, new System.IO.FileStream(saveFileDialog.FileName, System.IO.FileMode.Create));
+                    pdfWriter.PageEvent = new PdfPageEventHelper();
+                    document.Open();
+                    document.NewPage();
+                    var titleFont = FontFactory.GetFont("Arial", 18, iTextSharp.text.Font.BOLD);
+
+                    Paragraph title = new iTextSharp.text.Paragraph("Text Analyzer Report", titleFont);
+
+                    title.Alignment = Element.ALIGN_CENTER;
+
+                    document.Add(title);
+
+                    Paragraph dateTitle = new Paragraph("Date: " + DateTime.Today.ToString("yyyy/MM/dd"));
+                    title.Alignment = Element.ALIGN_RIGHT;
+
+                    document.Add(dateTitle);
+
+                    document.Add(new Paragraph("\n"));
+
+                    Paragraph textTitle = new Paragraph("Text: ");
+                   
+                    document.Add(textTitle);
+
+                    document.Add(new Paragraph("\n"));
+
+                    Paragraph text = new Paragraph(analyzerClass.GetString);
+
+                    document.Add(text);
+
+                    document.Add(new Paragraph("\n"));
+
+                    var font = FontFactory.GetFont("Arial", 18, iTextSharp.text.Font.NORMAL);
+
+                    Paragraph paragraph = new Paragraph("Text Analysis. Numeric data", font);
+                    paragraph.Alignment = Element.ALIGN_CENTER;
+                    document.Add(paragraph);
+
+                    document.Add(new Paragraph("\n"));
+
+                    PdfPTable pdfPTable = new PdfPTable(2);
+
+                    pdfPTable.AddCell("Number of words");
+                    pdfPTable.AddCell(analyzerClass.CalculateWordAmount().ToString());
+
+                    pdfPTable.AddCell("Number of letters");
+                    pdfPTable.AddCell(analyzerClass.CalculateLettersAmount().ToString());
+
+
+                    pdfPTable.AddCell("Number of digits");
+                    pdfPTable.AddCell(analyzerClass.CalculateDigitsAmount().ToString());
+
+                    pdfPTable.AddCell("Longest word");
+                    pdfPTable.AddCell(analyzerClass.GetLongWord().Item1);
+
+                    pdfPTable.AddCell("Shortest word");
+                    pdfPTable.AddCell(analyzerClass.GetShortWord().Item1);
+
+                    pdfPTable.AddCell("Average word length");
+                    pdfPTable.AddCell(analyzerClass.GetAverageWordLength().ToString());
+
+
+                    pdfPTable.AddCell("Number of spaces");
+                    pdfPTable.AddCell(analyzerClass.CalculateSpaceAmount().ToString());
+
+
+                    pdfPTable.AddCell("Number of conconants");
+                    pdfPTable.AddCell(analyzerClass.GetConsonantsAmount().ToString());
+
+                    pdfPTable.AddCell("Number of vowels");
+                    pdfPTable.AddCell(analyzerClass.GetVowelsAmount().ToString());
+
+                    pdfPTable.AddCell("Number of punctuation marks");
+                    pdfPTable.AddCell(analyzerClass.GetPunctuationMarksAmount().ToString());
+
+                    document.Add(pdfPTable);
+
+                    document.NewPage();
+
+                    var titleFont2 = FontFactory.GetFont("Arial", 18, iTextSharp.text.Font.BOLD);
+
+                    Paragraph title2 = new iTextSharp.text.Paragraph("Word Frequency", titleFont);
+
+                    title2.Alignment = Element.ALIGN_CENTER;
+
+                    document.Add(title2);
+
+                    document.Add(new Paragraph("\n"));
+
+
+                    PdfPTable pdfPTable2 = new PdfPTable(2);
+
+                    var wordFrequency = analyzerClass.GetWordsFrequency();
+
+                    pdfPTable2.AddCell("Word");
+                    pdfPTable2.AddCell("Frequency");
+
+                    foreach (var item in wordFrequency)
+                    {
+                        pdfPTable2.AddCell(item.Key);
+                        pdfPTable2.AddCell(item.Value.ToString());
+                    }
+
+                    document.Add(pdfPTable2);
+                    document.NewPage();
+
+                    var titleFont3 = FontFactory.GetFont("Arial", 18, iTextSharp.text.Font.BOLD);
+                    Paragraph newParagraph = new Paragraph("Character Frequency Per Word", titleFont3);
+
+                    newParagraph.Alignment = Element.ALIGN_CENTER;
+                    document.Add(newParagraph);
+
+                    document.Add(new Paragraph("\n"));
+
+                    PdfPTable pdfPTable3 = new PdfPTable(3);
+
+                    var map = analyzerClass.GetCharacterFrequencyPerWord();
+
+                    pdfPTable3.AddCell("Word");
+                    pdfPTable3.AddCell("Character");
+                    pdfPTable3.AddCell("Frequency");
+
+                    foreach (var item in map)
+                    {
+                        foreach (var item2 in item.Value)
+                        {
+                            pdfPTable3.AddCell(item.Key);
+                            pdfPTable3.AddCell(item2.Key.ToString());
+                            pdfPTable3.AddCell(item2.Value.ToString());
+                        }
+                    }
+
+                    document.Add(pdfPTable3);
+                    document.NewPage();
+
+                    var titleFont4 = FontFactory.GetFont("Arial", 18, iTextSharp.text.Font.BOLD);
+
+                    Paragraph newParagraph2 = new Paragraph("Punctuation Mark Frequency", titleFont4);
+
+                    newParagraph2.Alignment = Element.ALIGN_CENTER;
+                    document.Add(newParagraph2);
+                    document.Add(new Paragraph("\n"));
+
+                    PdfPTable pdfPTable4 = new PdfPTable(2);
+
+                    var punctuationMarkFrequency = analyzerClass.GetFrequencyOfPunctuationMarks();
+
+                    pdfPTable4.AddCell("Punctuation Mark");
+                    pdfPTable4.AddCell("Frequency");
+
+                    foreach (var item in punctuationMarkFrequency)
+                    {
+                        pdfPTable4.AddCell(item.Key.ToString());
+                        pdfPTable4.AddCell(item.Value.ToString());
+                    }
+
+                    document.Add(pdfPTable4);
+
+                    document.Close();
+                    pdfWriter.Close();
+
+                    System.Windows.MessageBox.Show("Report saved successfully!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
+
+
+
+
+
+                }
+            }
         }
     }
 }
